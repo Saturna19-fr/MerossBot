c1032dde8de6b9030cf23157312c7a87fab3bf8d
Saturna19-fr <saturna19.stream@outlook.com>
2024-01-18 13:43:55 +0100

    Ajout logtype

--- utils/loggerservice.py
+++ utils/loggerservice.py
@@ -1,21 +1,21 @@
 from interactions import Client, Extension
-from utils.embeds import new_notify_embed
+from utils.embeds import new_embed
 class Logger():
     def __init__(self, client, channel = 0):
         self.client: Client = client
         self.channel = channel
 
-        self.LOGS_LEVEL = {"ERROR": "FF0000", "INFO": "0091FF", "CHANGE": "FFBF00"}
+        self.LOGS_LEVEL = {"ERROR": 0xFF0000, "INFO": 0x0091FF, "EDIT": 0xFFBF00}
 
     
-    async def sendLog(self, type, embed):
+    async def sendLog(self, logtype, embed):
         try:
-            color = self.LOGS_LEVEL["type"]
+            color = self.LOGS_LEVEL[logtype]
         except:
             color = "FFBF00"
         channel = await self.client.fetch_channel(self.channel)
         if channel:
-            await channel.send(embeds=[new_notify_embed(embed)])
+            await channel.send(embeds=[new_embed(title=logtype, description=embed, color=color)])
 
         
 
